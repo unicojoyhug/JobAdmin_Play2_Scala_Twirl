e32222a468fdd13c65f3673b7363831494586d11
Juyoung.Choi <juyoung.choi@jp.dk>
2017-11-09 12:38:25 +0100

    Delete Job with confirm using sweetalert implemented

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -2,27 +2,28 @@ var site = {
     init: function () {
         site.getSite();
         site.setDataTable();
+        site.setDeleteAlert();
     },
 
     getSite: function () {
         var name = "finanswatch.dk";
         var id = 1
-        $('#jobIframe').attr("src", "jobs/"+name);
-        $('#newjob').attr("href", "/jobs/newJob/"+name+ "/" + id);
-        $('#addJobBtn').attr("href", "/jobs/newJob/"+name+ "/" + id);
+        $('#jobIframe').attr("src", "jobs/" + name);
+        $('#newjob').attr("href", "/job/newJob/" + name + "/" + id);
+        $('#addJobBtn').attr("href", "/job/newJob/" + name + "/" + id);
 
         $('#site').change(function () {
-             name = $(this).find("option:selected").text().trim();
-             id = $(this).find("option:selected").val()
-            $('#jobIframe').attr("src", "jobs/"+name);
-            $('#newjob').attr("href", "/jobs/newJob/"+name+ "/" + id);
-            $('#addJobBtn').attr("href", "/jobs/newJob/"+name+ "/" + id);
+            name = $(this).find("option:selected").text().trim();
+            id = $(this).find("option:selected").val()
+            $('#jobIframe').attr("src", "jobs/" + name);
+            $('#newjob').attr("href", "/job/newJob/" + name + "/" + id);
+            $('#addJobBtn').attr("href", "/job/newJob/" + name + "/" + id);
 
         });
 
     },
 
-    setDataTable: function() {
+    setDataTable: function () {
         $('#joblistTable').DataTable({
             "language": {
                 "lengthMenu": "Vis _MENU_ jobs per side",
@@ -35,13 +36,37 @@ var site = {
                     "first": "Først side",
                     "next": "Næste",
                     "previous": "Før",
-                    "last":"Sidst"
+                    "last": "Sidst"
                 }
             },
-            "lengthMenu": [ [5, 10, 25, 50, -1], [5, 10, 25, 50, "All"] ]
+            "lengthMenu": [[5, 10, 25, 50, -1], [5, 10, 25, 50, "All"]]
+        });
+    },
+
+    setDeleteAlert: function () {
+        $('.btn-submit').on('click', function (e) {
+            e.preventDefault();
+            var form = $(this).parents('form');
+
+            swal({
+                title: "Er du sikker?",
+                text: "Jobbet vil blive slettet.",
+                icon: "warning",
+                buttons: true,
+                buttons: ["Annuller", "Slet"],
+                dangerMode: true,
+            }).then((willDelete) => {
+                if (willDelete) {
+                    form.submit();
+                    swal("Jobbet er slettet", {
+                        icon: "success",
+                    });
+                }else {
+                    swal("Annulleret", "Jobbet bliver ikke slettet.", "error");
+                }
+            });
         });
     }
-
 }
 
 
